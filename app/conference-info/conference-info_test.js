26f4313a05d43cd794cd3e2249a40547c61c8285
denhmorin <denishmorinski@gmail.com>
2017-12-28 19:20:51 +0100

    CREATED conference-info template, EDITED register events

---
+++ app/conference-info/conference-info_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.conference-info module', function() {
+
+  beforeEach(module('myApp.conference-info'));
+
+  describe('conference-info controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var ConferenceInfoCtrl = $controller('ConferenceInfoCtrl');
+      expect(ConferenceInfoCtrl).toBeDefined();
+    }));
+
+  });
+});
