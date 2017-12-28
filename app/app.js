3c4f77bbed9b3021e2627bbb1329a0e82cfd712a
Filip Vukovic <filipvukovic@ecx.io>
2017-12-28 02:04:07 +0100

    Fast and bad translate

--- app/app.js
+++ app/app.js
@@ -21,16 +21,70 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
 }]);
 app.config(['$translateProvider', function ($translateProvider) {
   $translateProvider.translations('en', {
-    'TITLE': 'Hello',
-    'FOO': 'This is a paragraph'
+    'HAVE_ACC': 'Already have an account',
+    'LOGIN': 'Login',
+    'DOWN_LINK':"You can download mobile app via link",
+    'MOBILE':'Mobile application',
+    'DOWNLOAD':"Download",
+    'COM_INFO':"Company info",
+    'NAME_COM':"Name of Company",
+    'ADDRESS':"Address",
+    'PHONE':"Phone",
+     //meni
+     'CONF_NAME':"Conference name",
+     "MAIN":"MAIN INFO",
+     "CONF_INF":"Conference info",
+     "LOC_HIS": "Location history",
+     "WEL_SPEC":"Welcome speech",
+     "REG_INFO":"REGITRATION INFO ",
+     "PRO_SER":"Products/Services registration",
+     "ABSTRACT":"ABSTRACT",
+     "LECTURES":"LECTURES",
+     "PRACTICAL":"PRACTICAL INFO",
+     "FORGET":"Forget the password",
+     "SIGN":"Sign in",
+     "REGISTER":"Register",
+     "ACCO":"Accomodation",
+     "HOW":"How to get there",
+     "SHOPPING":"Shopping",
+     "DINNING":"Fine dining"
+
   });
  
-  $translateProvider.translations('de', {
-    'TITLE': 'Hallo',
-    'FOO': 'Dies ist ein Absatz'
+  $translateProvider.translations('hr', {
+    'HAVE_ACC': 'Već imaš račun',
+    'LOGIN': 'Prijavi se',
+    'DOWN_LINK':" Možete skinuti mobilnu aplikaciju na sljedecem linku ",
+    'MOBILE':'Mobilna aplikacija',
+    'DOWNLOAD':"Preuzmi",
+    'COM_INFO':"Informacije o kompaniji",
+    'NAME_COM':"Ime kompanije",
+    'ADDRESS':"Adresa",
+    'PHONE':"Telefon", 
+
+      //meni
+      'CONF_NAME':"Naziv konferencije",
+      "MAIN":"GLAVNI INFO",
+      "CONF_INF":"Info konferencije",
+      "LOC_HIS": "Povijest lokacija",
+      "WEL_SPEC":"Govor dobrodošlice",
+      "REG_INFO":"REGISTRACIJA INFO ",
+      "PRO_SER":"Proizvodi/Servisi registracija",
+      "ABSTRACT":"ABSTRAKTNO",
+      "LECTURES":"PREDAVANJA",
+      "PRACTICAL":"PRAKIČNO INFO",
+      "FORGET":"Zaboravili lozinku",
+      "SIGN":"Prijavi se",
+      "REGISTER":"Registiraj se",
+      "ACCO":"Prijedlog",
+      "HOW":"Kako doći do tamo",
+      "SHOPPING":"Šoping",
+      "DINNING":"Fina jela"
   });
- 
-  $translateProvider.preferredLanguage('en');
+  if(window.localStorage.getItem("language")==null){
+    window.localStorage.setItem("language","en")
+  }
+  $translateProvider.preferredLanguage(window.localStorage.getItem("language"));
 }]);
 
   $("#input-id").fileinput();
--- app/home/home.js
+++ app/home/home.js
@@ -1,15 +1,20 @@
 'use strict';
 
-angular.module('myApp.home', ['ngRoute'])
+var app = angular.module('myApp.home', ['ngRoute', 'pascalprecht.translate'])
 
-.config(['$routeProvider', function($routeProvider,$scope) {
+app.config(['$routeProvider', function($routeProvider,$scope) {
   $routeProvider.when('/home', {
     templateUrl: 'home/home.html',
     controller: 'HomeCtrl'
   });
 }])
 
-.controller('HomeCtrl', function($scope) { 
+app.controller('HomeCtrl', function($scope,$translate) { 
   $scope.change = function(language){ 
+    $translate.use(language); 
+    window.localStorage.setItem("language",language);
+    
 }
 });
+
+ 
